e4645b7aa4c621384d4f22168ddd912003dbb42c
amart251 <amartinez251@miners.utep.edu>
2023-12-10 22:31:17 -0700

    created buzzer files; adjusted makefile to include wdt, buzzer; added buzzer functionality to lcddemo.c

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -5,6 +5,7 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+#include "buzzer.h"
 
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
@@ -12,12 +13,19 @@ main()
 {
   configureClocks();
   lcd_init();
+  buzzer_init();
+
   u_char width = screenWidth, height = screenHeight;
 
   clearScreen(COLOR_BLUE);
 
   drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
+  buzzer_set_period(1000); //single tone
+
+  //buzzer_set_period(0); //stop the buzzer
   
 }
